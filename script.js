da0b79bda07544f35ac67b7ea620d549fec8afc2
Aryan <159044932+ARYAN-WEBSITE@users.noreply.github.com>
2024-12-16 19:39:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+ 
+
+  import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+  
+  // Add your own Firebase config here
+ const firebaseconfig = {
+      apiKey: "AIzaSyC_iIo73TKTxXKRvUNOgMNI9TiZcWCGSeA",
+    authDomain: "aryan-website01.firebaseapp.com",
+    projectId: "aryan-website01",
+    storageBucket: "aryan-website01.firebasestorage.app",
+    messagingSenderId: "338929057688",
+    appId: "1:338929057688:web:2576140e9e1dde06a475ed",
+ }
+
+  
+
+
+
+  const app = initializeApp (firebaseconfig) ;
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider()
+
+  const signInButton = document.getElementById("signInButton");
+  const signOutButton = document.getElementById("signOutButton");
+  const message = document.getElementById("message");
+  const userName = document.getElementById("userName");
+  const userEmail = document.getElementById("userEmail");
+
+  signOutButton.style.display = "none";
+  message.style.display = "none";
+
+  const userSignIn = async() => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        const user = result.user
+        console.log(user);
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message
+    })
+  }
+
+  const userSignOut = async() => {
+    signOut(auth).then(() => {
+        alert("You have signed out successfully!");
+    }).catch((error) => {})
+  }
+
+  onAuthStateChanged(auth, (user) => {
+    if(user) {
+      signOutButton.style.display = "block";
+      message.style.display = "block";
+      userName.innerHTML = user.displayName;
+      userEmail.innerHTML = user.email
+    } else {
+      signOutButton.style.display = "none";
+      message.style.display = "none";
+    }
+  })
+
+  signInButton.addEventListener('click', userSignIn);
+  signOutButton.addEventListener('click', userSignOut);
